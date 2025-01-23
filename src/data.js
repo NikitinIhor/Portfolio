55e3d4015c4dcc46b2ce053be1b4a0b27a67ab37
Nikitin <nikitinigor0907@gmail.com>
2025-01-23 03:19:56 +0100

    add Family-Nest-Farm-Adventures

--- src/data.js
+++ src/data.js
@@ -1,4 +1,5 @@
 import ARTHROHARD from "../src/images/projects/ARTHROHARD.jpg";
+import familyNest from "../src/images/projects/Family-Nest-Farm-Adventures.jpg";
 import favorFood from "../src/images/projects/favorFood.jpg";
 import findpare from "../src/images/projects/findpare.jpg";
 import mygym from "../src/images/projects/gym.jpg";
@@ -17,7 +18,22 @@ import weatherDashboard from "../src/images/projects/weatherDashboard.jpg";
 
 export const DATA = [
   {
-    id: 2,
+    id: 3,
+    title: "Family-Nest-Farm-Adventures",
+    subtitle: "Command project",
+    skills:
+      "Responsive Design, JavaScript (ES6+), CSS3, UI/UX Design, Version Control",
+    image: familyNest,
+    alt: "Family-Nest-Farm-Adventures",
+    description:
+      "Family-Nest: Farm Adventures is a farming simulation game where players grow crops, raise animals, and complete exciting quests. Build your farm, enjoy heartwarming stories, and embark on fun adventures!",
+    url: "https://calm-brioche-ab3664.netlify.app/",
+    isNew: true,
+    year: "2025",
+    filter: "vanilla",
+  },
+  {
+    id: 4,
     title: "Nanny.Services",
     subtitle: "Personal project",
     skills:
@@ -32,7 +48,7 @@ export const DATA = [
     filter: "ts",
   },
   {
-    id: 3,
+    id: 5,
     title: "FavorFood",
     subtitle: "Personal project",
     skills:
@@ -47,7 +63,7 @@ export const DATA = [
     filter: "react",
   },
   {
-    id: 4,
+    id: 6,
     title: "WaterTracker",
     subtitle: "Command project",
     skills:
@@ -62,7 +78,22 @@ export const DATA = [
     filter: "react",
   },
   {
-    id: 5,
+    id: 7,
+    title: "ARTHROHARD",
+    subtitle: "Personal project",
+    skills:
+      "Responsive Design, JavaScript (ES6+), CSS3, UI/UX Design, Version Control",
+    image: ARTHROHARD,
+    alt: "ARTHROHARD",
+    description:
+      "Arthrohard is a highly digestible dietary supplement in the form of a syrup, created with the health of your dog's or cat's joints in mind. Our mobile app allows quick access to information about the product, its ingredients and health benefits, as well as allows interaction with various product presentation options.",
+    url: "https://nikitinihor.github.io/ARTHROHARD/",
+    isNew: true,
+    year: "2024",
+    filter: "vanilla",
+  },
+  {
+    id: 8,
     title: "TravelTracks",
     subtitle: "Personal project",
     skills:
@@ -72,110 +103,11 @@ export const DATA = [
     description:
       "Is a travel companion app that helps users find campers in any region. With advanced filters, users can refine their search to match specific needs, from camper type to amenities. The app offers captivating images for each camper, allowing users to browse visually. An easy-to-use order form streamlines booking, making it a convenient tool for planning road trips and adventures.",
     url: "https://travel-trucks-amber.vercel.app/",
-    isNew: true,
-    year: "2024",
-    filter: "react",
-  },
-  {
-    id: 6,
-    title: "Weather application",
-    subtitle: "Personal project",
-    skills:
-      "React.js, Redux, Redux Persist, Axios, API Integration, Responsive Design, JavaScript (ES6+), CSS3, UI/UX Design, Asynchronous Programming, Version Control",
-    image: weatherDashboard,
-    alt: "Weather-Dashboard-application",
-    description:
-      "A straightforward weather app that lets you enter any city and instantly receive up-to-date weather information. From current temperature and wind speed to other essential weather details, WeatherWatch keeps you informed with just a few taps. Perfect for planning your day or your travels, it offers a quick and easy way to stay on top of the weather.",
-    url: "https://weather-dashboard-application-vert.vercel.app/",
-    year: "2024",
-    filter: "react",
-  },
-  {
-    id: 7,
-    title: "PizzaMaker",
-    subtitle: "Personal project",
-    skills:
-      "React.js, Responsive Design, JavaScript (ES6+), CSS3, Animations, UI/UX Design, Version Control",
-    image: pizzaMaker,
-    alt: "multikino",
-    description:
-      "An interactive web app where users can customize their pizza by choosing different halves, each with its own price. After selecting, the order is animated into the cart, where users can either finalize the purchase or cancel. The app offers a fun and easy way to create and manage custom pizza orders.",
-    url: "https://pizzamaker-six.vercel.app/",
     year: "2024",
     filter: "react",
   },
   {
     id: 8,
-    title: "MultiKino",
-    subtitle: "Personal project",
-    skills:
-      "React.js, Redux, Axios, API Integration, Responsive Design, JavaScript (ES6+), CSS3, UI/UX Design, Asynchronous Programming, Version Control",
-    image: multikino,
-    alt: "multikino",
-    description:
-      "A movie discovery platform built with React and Redux, integrating the Movie Database API. Users can search for films, view detailed information, and watch trailers. The app is fully responsive and efficiently handles state management for a smooth user experience.",
-    url: "https://goit-react-hw-05-one-theta.vercel.app/",
-    year: "2024",
-    filter: "react",
-  },
-  {
-    id: 9,
-    title: "List of users",
-    subtitle: "Personal project",
-    skills:
-      "React.js, Redux, Redux Persist, Axios, API Integration, Responsive Design, JavaScript (ES6+), CSS3, UI/UX Design, Asynchronous Programming, Version Control",
-    image: usersList,
-    alt: "usersList",
-    description:
-      "A React and Redux app with persistent state, allowing users to add, update, and delete profiles seamlessly. It ensures data continuity across sessions with Redux Persist and features a clean, responsive design for easy user management.",
-    url: "https://my-user-list.vercel.app/",
-    year: "2023",
-    filter: "react",
-  },
-  {
-    id: 10,
-    title: "Phone book",
-    subtitle: "Personal project",
-    skills:
-      "React.js, Redux, Redux Persist, Axios, API Integration, Responsive Design, JavaScript (ES6+), CSS3, UI/UX Design, Asynchronous Programming, Version Control",
-    image: phonebook,
-    alt: "phonebook",
-    description:
-      "A React application utilizing Redux and Redux Persist for state management, featuring secure login and logout functionality. Users can efficiently manage their contacts with operations to add, update, and delete entries.",
-    url: "https://goit-react-hw-08-five-xi.vercel.app/",
-    year: "2023",
-    filter: "react",
-  },
-  {
-    id: 11,
-    title: "Portfolio Website",
-    subtitle: "Command project",
-    skills:
-      "Responsive Design, JavaScript (ES6+), CSS3, UI/UX Design, Version Control",
-    image: hello,
-    alt: "hello",
-    description:
-      "A collaborative team project, we developed a responsive portfolio website that showcases various projects, complete with JavaScript animations for enhanced user experience. My primary role was to create and implement a contact section, which included a fully functional form for users to send messages directly from the site.",
-    url: "https://vikto0004.github.io/browser-battalion-2.0/",
-    year: "2024",
-    filter: "vanilla",
-  },
-  {
-    id: 12,
-    title: "Health Food",
-    subtitle: "Command project",
-    skills:
-      "Responsive Design, JavaScript (ES6+), CSS3, UI/UX Design, Version Control",
-    image: organic,
-    alt: "organic",
-    description:
-      "A team project focused on promoting health food. My primary responsibility was developing and modifying a key section of the application, while also contributing to other areas of the project. The experience also strengthened my skills in teamwork and project management.",
-    url: "https://dr-jeky11.github.io/health-project/",
-    year: "2024",
-    filter: "vanilla",
-  },
-  {
-    id: 14,
     title: "ARTHROHARD",
     subtitle: "Personal project",
     skills:
@@ -189,7 +121,105 @@ export const DATA = [
     filter: "vanilla",
   },
   {
+    id: 9,
+    title: "Weather application",
+    subtitle: "Personal project",
+    skills:
+      "React.js, Redux, Redux Persist, Axios, API Integration, Responsive Design, JavaScript (ES6+), CSS3, UI/UX Design, Asynchronous Programming, Version Control",
+    image: weatherDashboard,
+    alt: "Weather-Dashboard-application",
+    description:
+      "A straightforward weather app that lets you enter any city and instantly receive up-to-date weather information. From current temperature and wind speed to other essential weather details, WeatherWatch keeps you informed with just a few taps. Perfect for planning your day or your travels, it offers a quick and easy way to stay on top of the weather.",
+    url: "https://weather-dashboard-application-vert.vercel.app/",
+    year: "2024",
+    filter: "react",
+  },
+  {
+    id: 10,
+    title: "PizzaMaker",
+    subtitle: "Personal project",
+    skills:
+      "React.js, Responsive Design, JavaScript (ES6+), CSS3, Animations, UI/UX Design, Version Control",
+    image: pizzaMaker,
+    alt: "multikino",
+    description:
+      "An interactive web app where users can customize their pizza by choosing different halves, each with its own price. After selecting, the order is animated into the cart, where users can either finalize the purchase or cancel. The app offers a fun and easy way to create and manage custom pizza orders.",
+    url: "https://pizzamaker-six.vercel.app/",
+    year: "2024",
+    filter: "react",
+  },
+  {
+    id: 11,
+    title: "MultiKino",
+    subtitle: "Personal project",
+    skills:
+      "React.js, Redux, Axios, API Integration, Responsive Design, JavaScript (ES6+), CSS3, UI/UX Design, Asynchronous Programming, Version Control",
+    image: multikino,
+    alt: "multikino",
+    description:
+      "A movie discovery platform built with React and Redux, integrating the Movie Database API. Users can search for films, view detailed information, and watch trailers. The app is fully responsive and efficiently handles state management for a smooth user experience.",
+    url: "https://goit-react-hw-05-one-theta.vercel.app/",
+    year: "2024",
+    filter: "react",
+  },
+  {
+    id: 12,
+    title: "Phone book",
+    subtitle: "Personal project",
+    skills:
+      "React.js, Redux, Redux Persist, Axios, API Integration, Responsive Design, JavaScript (ES6+), CSS3, UI/UX Design, Asynchronous Programming, Version Control",
+    image: phonebook,
+    alt: "phonebook",
+    description:
+      "A React application utilizing Redux and Redux Persist for state management, featuring secure login and logout functionality. Users can efficiently manage their contacts with operations to add, update, and delete entries.",
+    url: "https://goit-react-hw-08-five-xi.vercel.app/",
+    year: "2023",
+    filter: "react",
+  },
+  {
+    id: 13,
+    title: "Portfolio Website",
+    subtitle: "Command project",
+    skills:
+      "Responsive Design, JavaScript (ES6+), CSS3, UI/UX Design, Version Control",
+    image: hello,
+    alt: "hello",
+    description:
+      "A collaborative team project, we developed a responsive portfolio website that showcases various projects, complete with JavaScript animations for enhanced user experience. My primary role was to create and implement a contact section, which included a fully functional form for users to send messages directly from the site.",
+    url: "https://vikto0004.github.io/browser-battalion-2.0/",
+    year: "2024",
+    filter: "vanilla",
+  },
+  {
+    id: 14,
+    title: "Health Food",
+    subtitle: "Command project",
+    skills:
+      "Responsive Design, JavaScript (ES6+), CSS3, UI/UX Design, Version Control",
+    image: organic,
+    alt: "organic",
+    description:
+      "A team project focused on promoting health food. My primary responsibility was developing and modifying a key section of the application, while also contributing to other areas of the project. The experience also strengthened my skills in teamwork and project management.",
+    url: "https://dr-jeky11.github.io/health-project/",
+    year: "2024",
+    filter: "vanilla",
+  },
+  {
     id: 15,
+    title: "List of users",
+    subtitle: "Personal project",
+    skills:
+      "React.js, Redux, Redux Persist, Axios, API Integration, Responsive Design, JavaScript (ES6+), CSS3, UI/UX Design, Asynchronous Programming, Version Control",
+    image: usersList,
+    alt: "usersList",
+    description:
+      "A React and Redux app with persistent state, allowing users to add, update, and delete profiles seamlessly. It ensures data continuity across sessions with Redux Persist and features a clean, responsive design for easy user management.",
+    url: "https://my-user-list.vercel.app/",
+    year: "2023",
+    filter: "react",
+  },
+  {
+    id: 16,
     title: "Notebook",
     subtitle: "Personal project",
     skills:
@@ -203,7 +233,7 @@ export const DATA = [
     filter: "vanilla",
   },
   {
-    id: 16,
+    id: 17,
     title: "Find a pair",
     subtitle: "Personal project",
     skills:
@@ -217,7 +247,7 @@ export const DATA = [
     filter: "vanilla",
   },
   {
-    id: 17,
+    id: 18,
     title: "Personal wallet",
     subtitle: "Personal project",
     skills:
@@ -231,7 +261,7 @@ export const DATA = [
     filter: "vanilla",
   },
   {
-    id: 18,
+    id: 19,
     title: "My Gym",
     subtitle: "Personal project",
     skills:
