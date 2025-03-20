d785b501133baead9bdbe43d47b55570f6902d12
Nikitin <nikitinigor0907@gmail.com>
2025-03-20 20:56:21 +0100

    feat:new project super match

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,3 +1,4 @@
+import { FaGithub } from "react-icons/fa";
 import css from "./Project.module.css";
 
 export default function Project({
@@ -9,12 +10,22 @@ export default function Project({
     alt = "project image",
     description,
     url,
+    github,
     isNew,
     year,
+    inProcess,
   },
 }) {
   return (
     <div className={css.wrapper}>
+      {inProcess && (
+        <div className={css.inProcess}>
+          <p> Application in Development</p>
+        </div>
+      )}
+      <a className={css.github} href={github} target="_blank">
+        <FaGithub /> <span>GitHub</span>
+      </a>
       {isNew && <div className={css.newBadge}>New</div>}
       <a href={url} className={css.item} target="_blank">
         <h2 className={css.title}>- {title} -</h2>
--- src/data.js
+++ src/data.js
@@ -10,7 +10,9 @@ import notebook from "../src/images/projects/notebook.jpg";
 import organic from "../src/images/projects/organic.jpg";
 import phonebook from "../src/images/projects/phonebook.jpg";
 import pizzaMaker from "../src/images/projects/pizzaMaker.jpg";
+import readJourney from "../src/images/projects/read-jourbey.jpg";
 import roin_group from "../src/images/projects/roin_group.jpg";
+import superMatch from "../src/images/projects/super-match.jpg";
 import travelTracks from "../src/images/projects/travelTracks.jpg";
 import usersList from "../src/images/projects/userslist.jpg";
 import wallet from "../src/images/projects/wallet.jpg";
@@ -19,7 +21,24 @@ import weatherDashboard from "../src/images/projects/weatherDashboard.jpg";
 
 export const DATA = [
   {
-    id: 2,
+    id: 1.1,
+    title: "READ JOURNEY",
+    subtitle: "Personal project",
+    skills:
+      "React.js, Redux, TS, Responsive Design, JavaScript (ES6+), CSS3, UI/UX Design, Asynchronous Programming, Version Control",
+    image: readJourney,
+    alt: "read journey",
+    description:
+      "READ JOURNEY is an app where you can find, add, and read books, filter selections, track your reading progress, and build your personal library with ease!",
+    url: "https://read-journey-taupe.vercel.app/recommended",
+    github: "https://github.com/NikitinIhor/read-journey",
+    isNew: true,
+    year: "2025",
+    filter: "react",
+    inProcess: true,
+  },
+  {
+    id: 1,
     title: "Roin Group",
     subtitle: "Command project",
     skills:
@@ -29,9 +48,26 @@ export const DATA = [
     description:
       "Roin Group is a website where you can search, choose, and order shower cabins, mirrors, glass partitions, terraces, canopies, and other glass products. You can customize your selection by width, price, and colors to create your ideal bathroom",
     url: "https://roin.group/",
+    github: "https://github.com/amyroniuk/next-my-app",
     isNew: true,
     year: "2025",
     filter: "next",
+  },
+  {
+    id: 2,
+    title: "Super Match",
+    subtitle: "Command project",
+    skills:
+      "Responsive Design, JavaScript (ES6+), CSS3, UI/UX Design, Version Control",
+    image: superMatch,
+    alt: "Super Match",
+    description:
+      "Super Match is an exciting match-3 game where you can team up with unique partners, use powerful boosters, compete in tournaments, and challenge friends in thrilling PvP battles. Join now for endless fun and rewards!",
+    url: "https://effortless-sable-63651b.netlify.app/",
+    github: "https://github.com/NikitinIhor/Super-Match",
+    isNew: true,
+    year: "2025",
+    filter: "vanilla",
   },
   {
     id: 3,
@@ -44,6 +80,7 @@ export const DATA = [
     description:
       "Family-Nest: Farm Adventures is a farming simulation game where players grow crops, raise animals, and complete exciting quests. Build your farm, enjoy heartwarming stories, and embark on fun adventures!",
     url: "https://calm-brioche-ab3664.netlify.app/",
+    github: "https://github.com/NikitinIhor/Family-Nest-Farm-Adventures",
     isNew: true,
     year: "2025",
     filter: "vanilla",
@@ -59,6 +96,7 @@ export const DATA = [
     description:
       "Arthrohard is a highly digestible dietary supplement in the form of a syrup, created with the health of your dog's or cat's joints in mind. Our mobile app allows quick access to information about the product, its ingredients and health benefits, as well as allows interaction with various product presentation options.",
     url: "https://nikitinihor.github.io/ARTHROHARD/",
+    github: "https://github.com/NikitinIhor/ARTHROHARD",
     isNew: true,
     year: "2024",
     filter: "vanilla",
@@ -74,7 +112,7 @@ export const DATA = [
     description:
       "Welcome to FovorFood, your one-stop destination for delicious food! Browse through our diverse menu of dinner options, cakes, salads, and more. Effortlessly search for your favorites, add them to your cart, and place your order with just a few clicks.",
     url: "https://favor-food.vercel.app/",
-    isNew: true,
+    github: "https://github.com/NikitinIhor/FavorFood",
     year: "2024",
     filter: "react",
   },
@@ -89,7 +127,8 @@ export const DATA = [
     description:
       "Stay hydrated and monitor your water intake with our user-friendly Water Tracker app. Users can easily log in to set daily water goals and select the amount of water they drink throughout the day. The app also features a history section, allowing users to review their daily, weekly, and monthly hydration habits. With reminders and insights, our Water Tracker helps you develop healthier hydration habits and achieve your wellness goals!",
     url: "https://browser-battalion-3-0-dev.vercel.app/welcome",
-    isNew: true,
+    github: "https://github.com/YevichDmytro/Water-traker",
+    github2: "https://github.com/NikitinIhor/WaterTracker-backend",
     year: "2024",
     filter: "react",
   },
@@ -104,6 +143,7 @@ export const DATA = [
     description:
       "NannyServices allows users to register, log in, and browse a list of nannies. They can filter nannies by price, rating, and availability, and contact them via a built-in form.",
     url: "https://nanny-services-eta.vercel.app/",
+    github: "https://github.com/NikitinIhor/Nanny.Services",
     year: "2024",
     filter: "ts",
   },
@@ -118,6 +158,7 @@ export const DATA = [
     description:
       "Is a travel companion app that helps users find campers in any region. With advanced filters, users can refine their search to match specific needs, from camper type to amenities. The app offers captivating images for each camper, allowing users to browse visually. An easy-to-use order form streamlines booking, making it a convenient tool for planning road trips and adventures.",
     url: "https://travel-trucks-amber.vercel.app/",
+    github: "https://github.com/NikitinIhor/TravelTrucks",
     year: "2024",
     filter: "react",
   },
@@ -132,6 +173,7 @@ export const DATA = [
     description:
       "A straightforward weather app that lets you enter any city and instantly receive up-to-date weather information. From current temperature and wind speed to other essential weather details, WeatherWatch keeps you informed with just a few taps. Perfect for planning your day or your travels, it offers a quick and easy way to stay on top of the weather.",
     url: "https://weather-dashboard-application-vert.vercel.app/",
+    github: "https://github.com/NikitinIhor/Weather-Dashboard-application",
     year: "2024",
     filter: "react",
   },
@@ -146,6 +188,7 @@ export const DATA = [
     description:
       "An interactive web app where users can customize their pizza by choosing different halves, each with its own price. After selecting, the order is animated into the cart, where users can either finalize the purchase or cancel. The app offers a fun and easy way to create and manage custom pizza orders.",
     url: "https://pizzamaker-six.vercel.app/",
+    github: "https://github.com/NikitinIhor/Pizzamaker",
     year: "2024",
     filter: "react",
   },
@@ -160,6 +203,7 @@ export const DATA = [
     description:
       "A movie discovery platform built with React and Redux, integrating the Movie Database API. Users can search for films, view detailed information, and watch trailers. The app is fully responsive and efficiently handles state management for a smooth user experience.",
     url: "https://goit-react-hw-05-one-theta.vercel.app/",
+    github: "https://github.com/NikitinIhor/MultiKino",
     year: "2024",
     filter: "react",
   },
@@ -174,6 +218,7 @@ export const DATA = [
     description:
       "A React application utilizing Redux and Redux Persist for state management, featuring secure login and logout functionality. Users can efficiently manage their contacts with operations to add, update, and delete entries.",
     url: "https://goit-react-hw-08-five-xi.vercel.app/",
+    github: "https://github.com/NikitinIhor/PhoneBook",
     year: "2023",
     filter: "react",
   },
@@ -188,6 +233,7 @@ export const DATA = [
     description:
       "A collaborative team project, we developed a responsive portfolio website that showcases various projects, complete with JavaScript animations for enhanced user experience. My primary role was to create and implement a contact section, which included a fully functional form for users to send messages directly from the site.",
     url: "https://vikto0004.github.io/browser-battalion-2.0/",
+    github: "https://github.com/Vikto0004/browser-battalion-2.0",
     year: "2024",
     filter: "vanilla",
   },
@@ -202,6 +248,7 @@ export const DATA = [
     description:
       "A team project focused on promoting health food. My primary responsibility was developing and modifying a key section of the application, while also contributing to other areas of the project. The experience also strengthened my skills in teamwork and project management.",
     url: "https://dr-jeky11.github.io/health-project/",
+    github: "https://github.com/dr-jeky11/health-project",
     year: "2024",
     filter: "vanilla",
   },
@@ -216,6 +263,7 @@ export const DATA = [
     description:
       "A React and Redux app with persistent state, allowing users to add, update, and delete profiles seamlessly. It ensures data continuity across sessions with Redux Persist and features a clean, responsive design for easy user management.",
     url: "https://my-user-list.vercel.app/",
+    github: "https://github.com/NikitinIhor/My_user_list",
     year: "2023",
     filter: "react",
   },
@@ -230,6 +278,7 @@ export const DATA = [
     description:
       "A vanilla JavaScript application that enables users to create and manage notes with descriptions and categories. The app offers a straightforward interface for adding, organizing, and viewing notes.",
     url: "https://nikitinihor.github.io/my-note/",
+    github: "https://github.com/NikitinIhor/my-note",
     year: "2022",
     filter: "vanilla",
   },
@@ -244,6 +293,7 @@ export const DATA = [
     description:
       "A vanilla JavaScript game where players find and match pairs of animal cards. The game features a simple and engaging interface, challenging players to improve their memory and attention skills as they uncover and match the animal pairs.",
     url: "https://nikitinihor.github.io/game-find-pair/",
+    github: "https://github.com/NikitinIhor/game-find-pair",
     year: "2022",
     filter: "vanilla",
   },
@@ -258,6 +308,7 @@ export const DATA = [
     description:
       "A dynamic wallet application built with Vanilla JavaScript. It allows users to add money, track income and expenses, and manage transactions, all with smooth, engaging animations for an enhanced user experience.",
     url: "https://nikitinihor.github.io/wallet/",
+    github: "https://github.com/NikitinIhor/wallet",
     year: "2022",
     filter: "vanilla",
   },
@@ -272,6 +323,7 @@ export const DATA = [
     description:
       "A lightweight gym management app built with vanilla JavaScript. It allows users to create, modify, and manage workout routines with ease.",
     url: "https://nikitinihor.github.io/My-Gym/",
+    github: "https://github.com/NikitinIhor/My-Gym",
     year: "2022",
     filter: "vanilla",
   },
